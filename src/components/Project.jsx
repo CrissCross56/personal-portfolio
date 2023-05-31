91b0123b7d14e39595e268618bf2a7bb6ccd2e8a
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-31 17:58:58 -0400

    added more jsx to portfolio projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,9 +2,15 @@ import { motion } from "framer-motion";
 import styles from "../CSS/test.module.css"
 export default function Project(){
     return(
-        <motion.div>
-            <p className={styles.text}> My first project at General assembly, a game of hangman <a href="https://classroomhangman.netlify.app/">https://classroomhangman.netlify.app/</a></p>
-            <p className={styles.text}>The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon. <a href="https://banana-bunch-digi-deck.netlify.app/cardsets">https://banana-bunch-digi-deck.netlify.app/cardsets</a></p>
-        </motion.div>
+        <div>
+            <div className={styles.project}>
+                <p>My first project at General assembly, a game of hangman <a href="https://classroomhangman.netlify.app/">https://classroomhangman.netlify.app/</a></p>
+            </div>
+           
+           <div className={styles.project}>
+                <p>The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon. <a href="https://banana-bunch-digi-deck.netlify.app/cardsets">https://banana-bunch-digi-deck.netlify.app/cardsets</a></p>
+           </div>
+           
+        </div>
     )
 }
