2934dff310d2f41ae7cc8b3b8eb5809ba777a958
CrissCross56 <grant.terdoslavich@gmail.com>
2023-06-02 14:49:18 -0400

    styled up the portfolio screen component as well as the project components nested inside there

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,25 +4,18 @@ import digimon from "../assets/digimonProj.png"
 // import hangman from "../assets/hangmanProj.png"
 export default function Project(props){
     return(
-        <div>
+        
             <div className={styles.project}>
-                <img src={props.photo} className={styles.hangmanImg}></img>
-                <p>{props.description}</p>
+                <img src={props.photo} className={styles.photo}></img>
+                <p className={styles.description}>{props.description}</p>
                 <ul>
                     <li>Hosted Domain: <a href={props.domain}>{props.domain}</a></li>
                     <li>Github Repo: <a href={props.repo}>{props.repo}</a></li>
                 </ul>
             </div>
            
-           {/* <div className={styles.project}>
-            <img src={digimon} className={styles.digimonImg}></img>
-                <p>The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon</p>
-                <ul>
-                    <li>Hosted Domain: <a href="https://banana-bunch-digi-deck.netlify.app/cardsets">https://banana-bunch-digi-deck.netlify.app/cardsets</a></li>
-                    <li>Github Repo: <a href=""></a></li>
-                </ul> 
-           </div> */}
+         
            
-        </div>
+        
     )
 }
--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -4,14 +4,16 @@ import Footer from "../components/Footer"
 import Project from "../components/Project"
 import hangman from "../assets/hangmanProj.png"
 import digimon from "../assets/digimonProj.png"
+import styles from "../CSS/test.module.css"
 export default function Portfolio(){
     const hangmanDomain = 'https://classroomhangman.netlify.app/';
     const digimonDomain = 'https://banana-bunch-digi-deck.netlify.app/';
     const hangmanRepo = 'https://github.com/CrissCross56/Front-End-Game'; 
     const digimonRepo = '';
     return(
-        <div>
+        <div className={styles.projectContainer}>
             <Project description={'My first project at General assembly, a game of hangman.'} domain={hangmanDomain} repo={hangmanRepo} photo={hangman}/>
+            <Project description={'The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon'} domain={digimonDomain} repo={digimonRepo} photo={digimon}></Project>
         </div>
     )
 }
