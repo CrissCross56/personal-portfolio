24bdc58f1e3ecaf6543498c929f459cd8ba15242
CrissCross56 <grant.terdoslavich@gmail.com>
2023-06-02 09:46:11 -0400

    changed portfolio and project components so i can use 2 project components and pass in props for their detailed description

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,27 @@
 import { motion } from "framer-motion";
 import styles from "../CSS/test.module.css"
-
-export default function Project(){
+import digimon from "../assets/digimonProj.png"
+// import hangman from "../assets/hangmanProj.png"
+export default function Project(props){
     return(
         <div>
             <div className={styles.project}>
-                <img></img>
-                <p>My first project at General assembly, a game of hangman <a href="https://classroomhangman.netlify.app/">https://classroomhangman.netlify.app/</a></p>
+                <img src={props.photo} className={styles.hangmanImg}></img>
+                <p>{props.description}</p>
+                <ul>
+                    <li>Hosted Domain: <a href={props.domain}>{props.domain}</a></li>
+                    <li>Github Repo: <a href={props.repo}>{props.repo}</a></li>
+                </ul>
             </div>
            
-           <div className={styles.project}>
-            <img></img>
-                <p>The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon. <a href="https://banana-bunch-digi-deck.netlify.app/cardsets">https://banana-bunch-digi-deck.netlify.app/cardsets</a></p>
-           </div>
+           {/* <div className={styles.project}>
+            <img src={digimon} className={styles.digimonImg}></img>
+                <p>The first group project I worked on at General Assembly, a deck builder website where users can build decks for the trading card game, Digimon</p>
+                <ul>
+                    <li>Hosted Domain: <a href="https://banana-bunch-digi-deck.netlify.app/cardsets">https://banana-bunch-digi-deck.netlify.app/cardsets</a></li>
+                    <li>Github Repo: <a href=""></a></li>
+                </ul> 
+           </div> */}
            
         </div>
     )
--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -2,12 +2,16 @@
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import Project from "../components/Project"
+import hangman from "../assets/hangmanProj.png"
+import digimon from "../assets/digimonProj.png"
 export default function Portfolio(){
+    const hangmanDomain = 'https://classroomhangman.netlify.app/';
+    const digimonDomain = 'https://banana-bunch-digi-deck.netlify.app/';
+    const hangmanRepo = 'https://github.com/CrissCross56/Front-End-Game'; 
+    const digimonRepo = '';
     return(
         <div>
-           
-            <Project/>
-          
+            <Project description={'My first project at General assembly, a game of hangman.'} domain={hangmanDomain} repo={hangmanRepo} photo={hangman}/>
         </div>
     )
 }
