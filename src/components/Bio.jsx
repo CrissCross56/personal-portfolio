184a2e8acfe191b06b908e955f54890e9685d069
CrissCross56 <grant.terdoslavich@gmail.com>
2023-06-02 22:09:13 -0400

    last minute addition of resume and styles

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,7 +5,7 @@ export default function Bio(){
     return(
         <div>
             <img src={pic} className={styles.img}/>
-            <p className={styles.bio}>Hi, I'm Grant Terdoslavich, an NYC dev. With knowledge of webdev fundamentals, such as HTML,CSS,JS, and frameworks like React, I aim to create interactive user experiences with my work. Here's my <a href={resume}>resume</a></p>
+            <p className={styles.bio}>Hi, I'm Grant Terdoslavich, an NYC dev. With knowledge of webdev fundamentals, such as HTML,CSS,JS, and frameworks like React, I aim to create interactive user experiences with my work. Here's my <a href={resume}>resume</a>.  If you want to shoot me a message, you can send me an email at grant.terdoslavich@gmail.com or check me out at the social links below.</p>
         </div>
     )
 }
