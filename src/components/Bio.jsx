1ecf37c8781d31464c57cc705a713ad7b52496d8
CrissCross56 <grant.terdoslavich@gmail.com>
2023-06-02 22:03:38 -0400

    added icon links to my linked in and github, added a link to my resume

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,10 +1,11 @@
 import styles from "../CSS/test.module.css"
 import pic from "../assets/grant.jpg"
+import resume from "../assets/GrantResume.pdf"
 export default function Bio(){
     return(
         <div>
             <img src={pic} className={styles.img}/>
-            <p className={styles.bio}>Hi, I'm Grant Terdoslavich, an NYC dev. With knowledge of webdev fundamentals, such as HTML,CSS,JS, and frameworks like React, I aim to create interactive user experiences with my work.</p>
+            <p className={styles.bio}>Hi, I'm Grant Terdoslavich, an NYC dev. With knowledge of webdev fundamentals, such as HTML,CSS,JS, and frameworks like React, I aim to create interactive user experiences with my work. Here's my <a href={resume}>resume</a></p>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import styles from '../CSS/test.module.css'
 export default function Footer(){
     return(
         <div className={styles.footer}>
+            <a href="https://www.linkedin.com/in/grant-terdoslavich-363134127/"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://github.com/CrissCross56"><i class="fa-brands fa-github"></i></a>
             <p className={styles.text}>2023 ©</p>
         </div>
     )
