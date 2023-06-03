c15a3472c7a5b2a072db645828a524418045ceb4
CrissCross56 <grant.terdoslavich@gmail.com>
2023-06-02 22:50:03 -0400

    added a comment

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,8 +4,9 @@ import digimon from "../assets/digimonProj.png"
 // import hangman from "../assets/hangmanProj.png"
 export default function Project(props){
     return(
-        
+            
             <div className={styles.project}>
+                {/* more to come later*/}
                 <img src={props.photo} className={styles.photo}></img>
                 <p className={styles.description}>{props.description}</p>
                 <ul>
