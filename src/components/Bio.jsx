4572f1a173dd44cb1927f1c4319bc17c6598cf8e
CrissCross56 <grant.terdoslavich@gmail.com>
2024-01-03 15:31:20 -0500

    changed the email tag in Bio component so it works

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -10,7 +10,7 @@ export default function Bio(){
             {/* <motion.img initial={{x:"-100vw"}} animate={{x:0}} transition={{stiffness:0}} src={profilePic} className={styles.img} alt="Grant"/> */}
             <motion.p initial={{opacity:0, x: "100vw"}} animate={{opacity:[0.2,0.4,0.6,0.8,1], x:0}} transition={{stiffness:0}} className={styles.bio}>            <motion.img initial={{x:"-100vw"}} animate={{x:0}} transition={{stiffness:0}} src={profilePic} className={styles.img} alt="Grant"/>
             Hi there! I'm Grant Terdoslavich, an NYC-based developer with a passion for crafting engaging online experiences. Armed with a solid foundation in web development fundamentals like HTML, CSS, and JavaScript, along with experience in frameworks like React, I strive to elevate digital interactions through my work. In my pursuit of creating delightful user experiences, I bring to the table methodical and persistent problem-solving skills, honed through years of object-oriented programming. Drawing inspiration from my experience running a Kendo club in college, effective communication is at the core of my approach.
-            Explore my professional journey further by checking out my projects in my portfolio, or send me an <a href="grant.terdoslavich@gmail.com">email</a>.</motion.p>
+            Explore my professional journey further by checking out my projects in my portfolio, or send me an <a href="mailto:grant.terdoslavich@gmail.com">email</a>.</motion.p>
             
             <div className={styles.container}>
             <NavLink to="/portfolio">
