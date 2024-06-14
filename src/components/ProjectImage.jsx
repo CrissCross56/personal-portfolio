4d0ff941c2d1b9d496da109703dac308db2f6a2f
CrissCross56 <grant.terdoslavich@gmail.com>
2024-06-14 03:33:46 -0400

    made the project/portfolio side of the site more responsive

--- src/components/ProjectImage.jsx
+++ src/components/ProjectImage.jsx
@@ -3,11 +3,11 @@ import styles from "../CSS/Project.module.css"
 export default function ProjectImage(props){
     return(
         <div>
-            <div>
+           
                 <a href={props.domain}>
                     <motion.img initial={{x:"-100vw"}} animate={{x:0}} transition={{stiffness:0}} whileHover={{scale:1.1}} src={props.photo} className={styles.photo} alt="project"></motion.img>
                 </a>
-            </div>
+            
         </div>
     )
 }
